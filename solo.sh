e91f1f0634c1968079e84aca58537ec4bef20647
caichuong00 <146566942+caichuong00@users.noreply.github.com>
2024-03-17 12:28:08 +0700

    Update solo.sh

--- solo.sh
+++ solo.sh
@@ -1,1 +1,1 @@
-./astrominer -w dero1qyyfrk0l5axv3vm0z8vqu8tjxyu78lxc5lzrsrmrtpk9q2msmlxdyqg3jgsmy.@solo -r dero-node-orionure-sg.mysrv.cloud:10300
+./astrominer -w dero1qyyfrk0l5axv3vm0z8vqu8tjxyu78lxc5lzrsrmrtpk9q2msmlxdyqg3jgsmy.@solo -r community-pools.mysrv.cloud:10300
