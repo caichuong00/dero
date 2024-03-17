c12fbd07ae6e6bfa5b54e9babbc5935b45386c85
caichuong00 <146566942+caichuong00@users.noreply.github.com>
2024-03-17 12:27:20 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,1 +1,1 @@
-./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqcuq6tkh2sla7r8n -r dero-node-orionure-sg.mysrv.cloud:10300
+./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqcuq6tkh2sla7r8n -r community-pools.mysrv.cloud:10300
