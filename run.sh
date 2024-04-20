dc77f923eb18206d98654dc232386ef07df16758
caichuong00 <146566942+caichuong00@users.noreply.github.com>
2024-04-20 22:44:21 +0700

    Update and rename run1.sh to run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+./astrominer -w dero1qy9vhund3tf6skcjvdf5rmm4puzaqxazac7p5lf2nx7udx9nc5jx5qgqcd069.@solo -r community-pools.mysrv.cloud:10300
--- run1.sh
+++
@@ -1,1 +0,0 @@
-./astrominer -w dero1qy9nmw4pxtdytw8mlqh4kqepd6v4nttquyqrzt3pkv0whvcqvqhf6qq88v3ny -r community-pools.mysrv.cloud:10300
