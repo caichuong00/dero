13f832c60deff43d62607ae56da51142897899b0
caichuong00 <146566942+caichuong00@users.noreply.github.com>
2024-04-21 20:43:51 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Good luck today !"
 while :; do
-./astrominer -r2 community-pools.mysrv.cloud:10300 -m 7 -r1 dero-node-yashnik-eu.mysrv.cloud -r dero-node-ch4k1pu.mysrv.cloud:10300 -w dero1qy9vhund3tf6skcjvdf5rmm4puzaqxazac7p5lf2nx7udx9nc5jx5qgqcd069.@solo 
+./astrominer -r2 community-pools.mysrv.cloud:10300 -m 7 -r1 dero-node-yashnik-eu.mysrv.cloud:10300 -r dero-node-ch4k1pu.mysrv.cloud:10300 -w dero1qy9vhund3tf6skcjvdf5rmm4puzaqxazac7p5lf2nx7udx9nc5jx5qgqcd069.@solo 
 sleep 5;
 done
