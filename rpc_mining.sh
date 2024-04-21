55c8547ba069b8c579ab26f8b2dff75f38ca8ff8
caichuong00 <146566942+caichuong00@users.noreply.github.com>
2024-04-21 21:11:47 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,1 +1,1 @@
-./astrominer -r2 community-pools.mysrv.cloud:10300 -m 7 -r1 dero-node-yashnik-eu.mysrv.cloud:10300 -r dero-node-ch4k1pu.mysrv.cloud:10300 -w dero1qy9vhund3tf6skcjvdf5rmm4puzaqxazac7p5lf2nx7udx9nc5jx5qgqcd069.@solo
+./astrominer -m 7 -r2 community-pools.mysrv.cloud:10300  -r1 dero-node-yashnik-eu.mysrv.cloud:10300 -r dero-node-ch4k1pu.mysrv.cloud:10300 -w dero1qy9vhund3tf6skcjvdf5rmm4puzaqxazac7p5lf2nx7udx9nc5jx5qgqcd069.@solo
