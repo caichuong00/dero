354d417bb9bc090ab476336af0269fce817b6437
caichuong00 <146566942+caichuong00@users.noreply.github.com>
2024-04-21 11:22:47 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,1 +1,6 @@
-./astrominer -w dero1qy9vhund3tf6skcjvdf5rmm4puzaqxazac7p5lf2nx7udx9nc5jx5qgqcd069.phone@solo -m 6 -r community-pools.mysrv.cloud:10300
+#!/bin/bash
+echo "Good luck today !"
+while :; do
+./astrominer -w dero1qy9vhund3tf6skcjvdf5rmm4puzaqxazac7p5lf2nx7udx9nc5jx5qgqcd069.phone@solo -r community-pools.mysrv.cloud:10300 -m 6
+sleep 9;
+done
